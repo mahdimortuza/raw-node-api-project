617761f4dbfcbe920619b906dc3d5053b74ec560
mahdimortuza <mahdi.webx@gmail.com>
2024-07-12 12:40:45 +0600

    route added

--- index.js
+++ index.js
@@ -8,28 +8,26 @@
 */
 
 // dependencies
-const http = require('http')
+const http = require('http');
+const { handleReqRes } = require('./helpers/handleReqRes');
 
 // app object - module scaffolding
-const app = {}
+const app = {};
 
 // configuration
 app.config = {
-    port: 3000
-}
+    port: 3000,
+};
 
 // create server
 app.createServer = () => {
-    const server = http.createServer(handleRequest)
+    const server = http.createServer(app.handleRequest);
     server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`)
-    })
-}
+        console.log(`listening to port ${app.config.port}`);
+    });
+};
 
 // handle request response
-app.handleRequest = (req, res) => {
-    // response handle
-    res.end('hello world ')
-}
+app.handleReqRes = handleReqRes;
 
-app.createServer()
+app.createServer();
