62dd9ca442ec6fdcea7e4ee5a895fe8e0eb9c5bd
mahdimortuza <mahdi.webx@gmail.com>
2024-07-14 10:45:36 +0600

    data saving logic added

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-/*
-
-* title: uptime monitoring application
-* description: a restful API to monitor up or down time of user defined links
-* author: mahdi
-* date: 12/06/24
-
-*/
-
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments')
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
-
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
