0abfc3cbc1ceeb616ba3210089c3de7293235600
mahdimortuza <mahdi.webx@gmail.com>
2024-07-14 11:34:09 +0600

    CRUD operation added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments')
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
+
+// testing file system
+// @TODO: pore muche dibo
+data.delete('test', 'newFile', (err) => {
+    console.log(err);
+});
 
 // create server
 app.createServer = () => {
